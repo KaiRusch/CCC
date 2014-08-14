3548b3744e72e1e2a92a786796d37bf317e8f231
KaiRusch <kai.rusch@gmail.com>
2014-08-14 17:49:10 -0400

    Update s3.cpp

--- 2014/s3/s3.cpp
+++ 2014/s3/s3.cpp
@@ -34,25 +34,31 @@ int main(int argc, char *argv[])
 			mountain.push(cart);
 		}
 
+		// 'Y' if all carts can make it to the lake 'N' if not
 		char state = 'Y';
 
+		//While the puzzle is still possible and has not been completed
 		while(state == 'Y' && lake != N)
 		{
+			//If the cart on the mountain can be moved to the lake, move it
 			if(mountain.top() == lake + 1)
 			{
 				lake++;
 				mountain.pop();
 			}
+			//If the carts on the branch can be moved to the lake move it
 			else if(branch.top() == lake + 1)
 			{
 				lake++;
 				branch.pop();
 			}
+			//If there is a cart on the mountain move it to the branch
 			else if(mountain.size() > 1)
 			{
 				branch.push(mountain.top());
 				mountain.pop();
 			}
+			//Otherwise it is impossible
 			else
 			{
 				state = 'N';
