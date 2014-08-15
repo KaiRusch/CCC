45f8fa34a0bcaaa4ce8e7296dfc4d63dc39c3b6e
KaiRusch <kai.rusch@gmail.com>
2014-08-15 10:55:25 -0400

    Completed 2011 s3

--- 2011/s3/s3.cpp
+++ 2011/s3/s3.cpp
@@ -4,6 +4,29 @@
 
 using namespace std;
 
+int magnify(int m, int x)
+{
+	if(m >= 1)
+	{
+		int power = pow(5,m-1);
+		int x2 = x/power;
+
+		if(x2 == 0 || x2 == 4)
+		{
+			return 0;
+		}
+		else if(x2 == 1 || x2 == 3)
+		{
+			return 1*power + magnify(m-1,x%power);
+		}
+		else if(x2 == 2)
+		{
+			return 2*power + magnify(m-1,x%power);
+		}
+	}
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	//Read the number of tests
@@ -15,7 +38,6 @@ int main(int argc, char *argv[])
 		//Read the magnification level
 		int m = 0;
 		scanf("%d",&m);
-		m = pow(5,m);
 		
 
 		//Read the x cooridinate
@@ -26,6 +48,14 @@ int main(int argc, char *argv[])
 		int y = 0;
 		scanf("%d",&y);
 
+		if(y < magnify(m,x))
+		{
+			printf("crystal\n");
+		}
+		else
+		{
+			printf("empty\n");
+		}
 
  
 	}
