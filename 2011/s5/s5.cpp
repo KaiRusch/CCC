1467bfa8433e6744bf833c95773811dcd90a29bc
KaiRusch <kai.rusch@gmail.com>
2014-08-22 19:24:33 -0400

    Update s5.cpp

--- 2011/s5/s5.cpp
+++ 2011/s5/s5.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+//"Approximation" of infinity
 const int infinity = 1000;
 
 int main()
@@ -75,31 +76,44 @@ int main()
 		int j = i;
 		while(j <= G && groups[j][1] - groups[i][0] <= 7)
 		{
-			//The span of the two groups
+			//The span of the groups
 			int span = groups[j][1] - groups[i][0];
 			//The number of lights which are on
-			int on = (groups[j][1] - groups[j][0])+(groups[i][1] - groups[i][0]);
+			int on = 0;
+			for(int k = i; k < j; ++k)
+			{
+				on += groups[k][1] - groups[k][0];
+			}
+			}
 			if(i == j)
 			{
 				on = span;
 			}
 			
+			//If the span of the groups is less than five it can always be solved
 			if(span <= 5)
 			{
+				//If the span is less than 4, set it to 4 for the answer
 				if(span < 4)
 				{
 					span = 4;
 				}
-
+				
+				//Calculate the minimum number of moves to turn the groups off
 				int minMove = span - on;
+				//If the minimum moves of these groups plus the groups to the right is less than
+				//The current minimum of this group set it
 				if(minMove + minMoves[j+1] < minMoves[i])
 				{
 					minMoves[i] = minMove + minMoves[j+1];
 				}
 			}
 
+			//If 5 < span <= 7, then it can possibly be turned of
 			else if(span <= 7)
 			{
+				//If the 4th light in a span of seven is off
+				//The minimum moves is span - on
 				if(span == 7 && lights[i+3] == 0)
 				{
 					int minMove = span - on;
@@ -108,6 +122,7 @@ int main()
 						minMoves[i] = minMove + minMoves[j+1];
 					}
 				}
+				//If the 3rd or 4th lights are off in a span of 6
 				else if(span ==6 && (lights[i+3] == 0 || lights[i+2] == 0))
 				{
 					int minMove = span - on;
@@ -118,6 +133,7 @@ int main()
 				}
 			}
 		
+			//Increment the 
 			j++;
 		}
 	}
