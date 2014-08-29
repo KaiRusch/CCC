02c3cdc484816cd37ed0d7e841b5c615dc66e0d2
KaiRusch <kai.rusch@gmail.com>
2014-08-29 16:03:28 -0400

    Completed 2010 s5, though it fails to correctly create the tree for the last three inputs

--- 2010/s5/s5.cpp
+++ 2010/s5/s5.cpp
@@ -1,52 +1,172 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-class Leaf
+enum Type{ROOT,BRANCH,LEAF};
+
+class Node
 {
 	public:
-		int nutrients;
+		
+		int ID;
 
-		Leaf(int n){nutrients = n;};
+		int type;
+
+		int production;
+
+		Node *parent;
+		Node *left;
+		Node *right;
+
+		Node(int id, int t, Node *par, int p){ID = id; type = t; production = p; parent = par; left = NULL; right = NULL;};
 };
 
-class Branch
+Node *root = NULL;
+
+int get_best_production(vector<vector<int> > &bestResults, Node *node, int growthAgents)
 {
-	public:
-		int capacity;
+	if(node == NULL)
+	{
+		return 0;
+	}
 
-		Branch *branchLeft;
-		Branch *branchRight;
+	int ID = node->ID;
 
-		Leaf *leafLeft;
-		Leaf *leafRight;
+	if(bestResults[ID][growthAgents] != 0)
+	{
+		return bestResults[ID][growthAgents];
+	}
 
-		Branch() {capacity = 1; branchLeft = NULL; branchRight = NULL; leafLeft = NULL; leafRight = NULL;}
-};
+	else if (node->type == LEAF)
+	{
+		int production = node->production + growthAgents;
+		bestResults[ID][growthAgents] = production;
+		return production;
+	}
 
-class Root
-{
-	public:
-		Branch *branchRight;
-		Branch *branchLeft;
+	else if(node->type == BRANCH)
+	{
+		int capacity = 1;
+		if(growthAgents == 0)
+		{
+			int production = get_best_production(bestResults,node->left,0) + get_best_production(bestResults,node->right,0);
+			if(production > capacity)
+			{
+				production = capacity;
+			}
+			
+			bestResults[ID][0] = production;
+			return production;
+		}
+		
+		else
+		{
+			int maxProduction = 0;
 
-		Root() {branchRight = NULL; branchLeft = NULL;};
-};
+			for(int i = 0; i <= growthAgents; ++i)
+			{
+				for(int j = 0; j <= growthAgents - i; ++j)
+				{
 
-Root root;
+					capacity = (1+i)*(1+i); 
+					int production = get_best_production(bestResults,node->left,j) + get_best_production(bestResults,node->right,growthAgents-j-i);
+					if(production > capacity)
+					{
+						production = capacity;
+					}
 
-void create_tree(bool rightBranch)
-{
+					if(production > maxProduction)
+					{
+						maxProduction = production;
+					}
+				}
+			}
 
+			bestResults[ID][growthAgents] = maxProduction;
+			return maxProduction;
+		}
+
+	}
+
+	else if(node->type == ROOT)
+	{
+		int maxProduction = 0;
+
+		for(int i = 0; i <= growthAgents; ++i)
+		{
+			int production = get_best_production(bestResults,node->left,i) + get_best_production(bestResults,node->right,growthAgents-i);
+			if(production > maxProduction)
+			{
+				maxProduction = production;
+			}
+		}
+
+		return maxProduction;
+	}
 }
 
 int main()
 {
-	bool rightBranch = false;
+	string treeDescription = " ";
+	getline(cin,treeDescription);
 
-	char input = ' ';
-	cin >> input;
-	
-	create_tree(input,rightBranch);
+	root = new Node(0,ROOT,NULL,0);
+
+	Node *current = root;
+
+	//The number of nodes in the tree
+	int nodes = 0;
+
+	for(int i = 0; i < treeDescription.size(); ++i)
+	{
+		char input =  treeDescription[i];
+
+		if(input == '(')
+		{
+			nodes++;
+			current->left = new Node(nodes,BRANCH,current,0);
+			current = current->left;
+		}
+
+		else if(input == ' ')
+		{
+			nodes++;
+			current = current->parent;
+			current->right = new Node(nodes,BRANCH,current,0);
+			current =  current->right;
+		}
+
+		else if(input == ')')
+		{
+			current = current->parent;
+		}
+
+		else
+		{
+			int leafProduction = 0;
+
+			while(input != '(' && input != ' ' && input != ')' && i < treeDescription.size())
+			{
+				leafProduction = leafProduction * 10 + (input - '0');
+				i++;
+				input = treeDescription[i];
+			}
+
+			i--;
+			nodes++;
+			current->left = new Node(nodes,LEAF,current,leafProduction);
+		}
+	}
+
+	int X = 0;
+	cin >> X;
+
+	vector<int> empty(X+1,0);
+	vector<vector<int> > bestAns(nodes+1,empty);
+
+	cout << get_best_production(bestAns,root,X) << endl;
+
 	return 0;
 }
