0a694084d4b91e23ec0ccfc3e6fb76419b3f4ee8
KaiRusch <kai.rusch@gmail.com>
2014-09-01 00:03:12 -0400

    Fix 2010 s5 algorithm to solve all inputs quickly

--- 2010/s5/s5.cpp
+++ 2010/s5/s5.cpp
@@ -4,105 +4,110 @@
 
 using namespace std;
 
-enum Type{ROOT,BRANCH,LEAF};
-
 class Node
 {
 	public:
 		
 		int ID;
-
-		int type;
-
 		int production;
 
 		Node *left;
 		Node *right;
 
-		Node(int id, int t, int p){ID = id; type = t; production = p; left = NULL; right = NULL;};
+		Node(int id, int p){ID = id; production = p; left = NULL; right = NULL;};
 };
 
 
 
-int get_best_production(vector<vector<int> > &bestResults, Node *node, int growthAgents)
+void get_best_production(vector<vector<int> > &bestResults, Node *node, int growthAgents)
 {
-	if(node == NULL)
+	if (node->left->left == NULL)
 	{
-		return 0;
-	}
-
-	int ID = node->ID;
-
-	if(bestResults[ID][growthAgents] != 0)
-	{
-		return bestResults[ID][growthAgents];
-	}
-
-	else if (node->type == LEAF)
-	{
-		int production = node->production + growthAgents;
-		bestResults[ID][growthAgents] = production;
-		return production;
-	}
-
-	else if(node->type == BRANCH)
-	{
-		int capacity = 1;
-		if(growthAgents == 0)
+		for(int i = 0; i <= growthAgents; ++i)
 		{
-			int production = get_best_production(bestResults,node->left,0) + get_best_production(bestResults,node->right,0);
-			if(production > capacity)
-			{
-				production = capacity;
-			}
-			
-			bestResults[ID][0] = production;
-			return production;
+			int production = node->left->production + i;
+			bestResults[node->ID][i] = production;
 		}
-		
-		else
-		{
-			int maxProduction = 0;
 
+		return;
+	}
+
+	else
+	{
+		int max = 0;
+		int temp = 0;
+
+		vector<int> leftBest(growthAgents+1,0);
+
+		if(node->left != NULL)
+		{
+			get_best_production(bestResults,node->left,growthAgents);
 			for(int i = 0; i <= growthAgents; ++i)
 			{
-				for(int j = 0; j <= growthAgents - i; ++j)
+				for(int j = 0; j <= i; ++j)
 				{
-
-					capacity = (1+i)*(1+i); 
-					int production = get_best_production(bestResults,node->left,j) + get_best_production(bestResults,node->right,growthAgents-j-i);
-					if(production > capacity)
+					int capacity = (1+j)*(1+j);
+					if(capacity < bestResults[node->left->ID][i-j])
 					{
-						production = capacity;
+						temp = capacity;
 					}
-
-					if(production > maxProduction)
+					else
 					{
-						maxProduction = production;
+						temp = bestResults[node->left->ID][i-j];
+					}
+					if(temp > max)
+					{
+						max = temp;
 					}
 				}
-			}
 
-			bestResults[ID][growthAgents] = maxProduction;
-			return maxProduction;
+				leftBest[i] = max;
+			}
 		}
 
-	}
+		vector<int> rightBest(growthAgents+1,0);
+		
+		if(node->right != NULL)
+		{
+			get_best_production(bestResults,node->right,growthAgents);
+			for(int i = 0; i <= growthAgents; ++i)
+			{
+				max = 0;
+				for(int j = 0; j <= i; ++j)
+				{
+					int capacity = (1+j)*(1+j);
+					if(capacity < bestResults[node->right->ID][i-j])
+					{
+						temp = capacity;
+					}
+					else
+					{
+						temp = bestResults[node->right->ID][i-j];
+					}
+					if(temp > max)
+					{
+						max = temp;
+					}
+				}
 
-	else if(node->type == ROOT)
-	{
-		int maxProduction = 0;
+				rightBest[i] = max;
+			}
+		}
 
 		for(int i = 0; i <= growthAgents; ++i)
 		{
-			int production = get_best_production(bestResults,node->left,i) + get_best_production(bestResults,node->right,growthAgents-i);
-			if(production > maxProduction)
+			max = 0;
+			for(int j = 0; j <= i; ++j)
 			{
-				maxProduction = production;
+				temp = leftBest[j] + rightBest[i-j];
+				if(temp > max)
+				{
+					max = temp;
+				}
 			}
-		}
 
-		return maxProduction;
+			bestResults[node->ID][i] = max;
+		}
 	}
 }
 
@@ -119,7 +124,7 @@ void create_tree(string description, Node *parent, int &nodes)
 		}
 
 		nodes++;
-		parent->left = new Node(nodes,LEAF,value);
+		parent->left = new Node(nodes,value);
 	}
 	else
 	{
@@ -161,9 +166,9 @@ void create_tree(string description, Node *parent, int &nodes)
 		rightDescription = description.substr(i+1,description.length()-i);
 	
 		nodes++;
-		parent->left = new Node(nodes,BRANCH,0);
+		parent->left = new Node(nodes,0);
 		nodes++;
-		parent->right = new Node(nodes,BRANCH,0);
+		parent->right = new Node(nodes,0);
 
 		create_tree(leftDescription,parent->left,nodes);
 		create_tree(rightDescription,parent->right,nodes);
@@ -177,7 +182,7 @@ int main()
 	string description = " ";
 	getline(cin,description);
 
-	root = new Node(0,ROOT,0);
+	root = new Node(0,0);
 
 	int nodes = 0;
 
@@ -189,7 +194,9 @@ int main()
 	vector<int> empty(X+1,0);
 	vector<vector<int> > bestAns(nodes+1,empty);
 
-	cout << get_best_production(bestAns,root,X) << endl;
+	get_best_production(bestAns,root,X);
+
+	cout << bestAns[root->ID][X] << endl;
 
 	return 0;
 }
